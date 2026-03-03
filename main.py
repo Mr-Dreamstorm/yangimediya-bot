0b5894c011e0acfd073af2b43afd9b867788ee5d
Mr-Dreamstorm <kimdur44@gmail.com>
2026-03-03 21:10:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
 import asyncio
-import logging
-import os
-
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from config import BOT_TOKEN
 
-TOKEN = os.getenv("BOT_TOKEN")
-
-if not TOKEN:
-    print("❌ BOT_TOKEN topilmadi!")
-    exit()
-
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=TOKEN)
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
-@dp.message(CommandStart())
-async def start_handler(message: Message):
-    await message.answer("🚀 Bot ishlayapti!")
+@dp.message()
+async def start(msg: types.Message):
+    await msg.answer("🚀 Bot ishlayapti!")
 
 async def main():
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
+if name == "main":
     asyncio.run(main())
