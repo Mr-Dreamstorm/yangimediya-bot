b3fc6189bbc4408195e7d67d8072424be1f5ac0b
Mr-Dreamstorm <kimdur44@gmail.com>
2026-03-03 21:09:49 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,3 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-SUPER_ADMIN = 5302627260
-
-REQUIRED_CHANNEL = "@Yangimediya"
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
