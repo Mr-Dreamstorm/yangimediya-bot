416748b1a4e74a311485735b968a1b0c07589196
Mr-Dreamstorm <kimdur44@gmail.com>
2026-03-03 21:22:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
 from config import BOT_TOKEN
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
+# ✅ START komandasi
+@dp.message(CommandStart())
+async def start_handler(message: types.Message):
+    await message.answer("🚀 Bot ishlayapti! /start ishladi")
+
+# oddiy message
 @dp.message()
-async def start(msg: types.Message):
-    await msg.answer("🚀 Bot ishlayapti!")
+async def echo(message: types.Message):
+    await message.answer("📩 Xabar keldi")
 
 async def main():
     await dp.start_polling(bot)
